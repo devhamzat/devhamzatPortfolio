22c6ae210aabe3cc83ad6b7439d8946be0874e64
devhamzat <ayomidelegbede@gmail.com>
2024-04-09 11:43:45 +0100

    tailwind css inluded

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				background: "#1B1212",
+			},
+		},
+	},
+	plugins: [],
+};
