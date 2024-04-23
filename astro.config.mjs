8a53c440f8b0d27e419bc7da06f73ccb7a13d5c7
devhamzat <ayomidelegbede@gmail.com>
2024-04-23 12:33:32 +0100

    new look,and added hub section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  publicDir: './src/images',
 });
